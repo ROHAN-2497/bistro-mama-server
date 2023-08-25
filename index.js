ae025719f782c5f092b9ab7570000c630838b01c
rohan future devoloper <xrohaislam373@gmail.com>
2023-08-26 02:54:26 +0600

    delete create

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xw5a4us.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -62,6 +62,12 @@ app.post('/carts', async (req, res) =>{
   res.send(result);
 })
 
+app.delete('/carts/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id) };
+  const result = await cartCollection.deleteOne(query);
+  res.send(result);
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
